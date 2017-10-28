26e37350f5f1c6da764b8acab766972b7dc502f5
Hanavan Kuhn <Hanavan99@gmail.com>
2017-10-28 01:05:39 -0500

    Basic resource classes for easier implementation.

---
+++ src/com/glutilities/resource/ResourceLoader.java
@@ -0,0 +1,37 @@
+package com.glutilities.resource;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Loads resources from files. This class has no functionality without
+ * implementation.
+ * 
+ * @author Hanavan99
+ *
+ * @param <R> The type of resource stored
+ * @param <K> The type of key used to uniquely identify each resource
+ */
+public abstract class ResourceLoader<R, K> {
+
+	/**
+	 * Load a resource from a file.
+	 * 
+	 * @param file The file path
+	 * @param key The key used to identify this resource
+	 * @return The resource
+	 * @throws IOException If there is some problem reading the file
+	 */
+	public abstract R load(File file, K key) throws IOException;
+
+	/**
+	 * Determines if a given resource is identified by this key. Usually the key
+	 * will be some unique value that is stored inside of the resource.
+	 * 
+	 * @param resource The resource to validate
+	 * @param key The key
+	 * @return If the resource is matched with the key
+	 */
+	public abstract boolean keyReferencesResource(R resource, K key);
+
+}
---
+++ src/com/glutilities/resource/ResourceManager.java
@@ -0,0 +1,75 @@
+package com.glutilities.resource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A wrapper class for the {@code ResourceLoader} class. Extends the
+ * functionality of the {@code ResourceLoader} to store any loaded objects for
+ * later reference by some key. To store loaded resources manually, use the
+ * {@code ResourceLoader} class, which only loads resources from external files.
+ * 
+ * @author Hanavan99
+ *
+ * @param <R> The type of resource stored
+ * @param <K> The type of key used to uniquely identify each resource
+ */
+public abstract class ResourceManager<R, K> {
+
+	private ResourceLoader<R, K> loader;
+	private List<R> resources = new ArrayList<R>();
+
+	/**
+	 * Constructor that accepts a {@code ResourceLoader} object for use when
+	 * loading resources. This constructor should be called in the subclass.
+	 * 
+	 * @param loader The {@code ResourceLoader} object to use for loading
+	 *            resources
+	 */
+	public ResourceManager(ResourceLoader<R, K> loader) {
+		this.loader = loader;
+	}
+
+	/**
+	 * Loads a resource from the specified file using the resource loader. Note
+	 * that if the loader's {@code load()} method throws an exception, the
+	 * resource will not be loaded.
+	 * 
+	 * @param file The path to the file
+	 * @param key The key to be used to reference the resource
+	 */
+	public void load(File file, K key) {
+		try {
+			loader.load(file, key);
+		} catch (IOException e) {
+
+		}
+	}
+
+	/**
+	 * Gets the resource identified by a key. If no resource is found,
+	 * {@code null} is returned.
+	 * 
+	 * @param key The key of the resource
+	 * @return The resource
+	 */
+	public R get(K key) {
+		for (R resource : resources) {
+			if (loader.keyReferencesResource(resource, key)) {
+				return resource;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Removes all resources that were stored internally. Once called, none of
+	 * the previous resources will be accessible, but future additions will be.
+	 */
+	public void deleteResources() {
+		resources.clear();
+	}
+
+}

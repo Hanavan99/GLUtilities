da2700d67e643d730e19fd25b88709ca92df098e
Hanavan99 <hanavan99@gmail.com>
2017-11-08 21:13:25 -0600

    Updated the shader program and shader object classes

--- src/com/glutilities/shader/ShaderObject.java
+++ src/com/glutilities/shader/ShaderObject.java
@@ -1,34 +1,96 @@
 package com.glutilities.shader;
 
+import org.lwjgl.opengl.ARBFragmentShader;
+import org.lwjgl.opengl.ARBGeometryShader4;
 import org.lwjgl.opengl.ARBShaderObjects;
+import org.lwjgl.opengl.ARBTessellationShader;
+import org.lwjgl.opengl.ARBVertexShader;
 
 import com.glutilities.core.Reusable;
 
+/**
+ * Represents a shader object.
+ * 
+ * @author Hanavan99
+ *
+ */
 public class ShaderObject implements Reusable {
 
+	/**
+	 * The shader ID.
+	 */
 	private int shader;
+
+	/**
+	 * The type of the shader.
+	 */
 	private int shaderType;
+
+	/**
+	 * The user-defined name of the shader.
+	 */
 	private String name;
+
+	/**
+	 * The code that is compiled into the shader program.
+	 */
 	private String code;
 
+	/**
+	 * Creates a new shader object with the specific shader type, name, and
+	 * code/program.
+	 * 
+	 * @param shaderType
+	 *            the type of shader
+	 * @param name
+	 *            the user-defined name of the shader; optional
+	 * @param code
+	 *            the code that is compiled into the shader program
+	 */
 	public ShaderObject(int shaderType, String name, String code) {
+		if (shaderType != ARBVertexShader.GL_VERTEX_SHADER_ARB && shaderType != ARBFragmentShader.GL_FRAGMENT_SHADER_ARB
+				&& shaderType != ARBGeometryShader4.GL_GEOMETRY_SHADER_ARB
+				&& shaderType != ARBTessellationShader.GL_TESS_CONTROL_SHADER
+				&& shaderType != ARBTessellationShader.GL_TESS_EVALUATION_SHADER) {
+			throw new IllegalArgumentException("Invalid shader type");
+		}
 		this.shaderType = shaderType;
 		this.name = name;
 		this.code = code;
 	}
 
+	/**
+	 * Gets the ID of the shader.
+	 * 
+	 * @return the ID
+	 */
 	public int getShaderID() {
 		return shader;
 	}
 
+	/**
+	 * Gets the type of shader.
+	 * 
+	 * @return the type of shader
+	 */
 	public int getShaderType() {
 		return shaderType;
 	}
 
+	/**
+	 * Gets the user-defined name of this shader; allowed to be null
+	 * 
+	 * @return the name of the shader
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Gets the code that was compiled into the shader program.
+	 * 
+	 * @return the code
+	 */
 	public String getShader() {
 		return code;
 	}
@@ -37,12 +99,10 @@ public class ShaderObject implements Reusable {
 	public void create() {
 		this.shader = ARBShaderObjects.glCreateShaderObjectARB(shaderType);
 	}
-	
+
 	@Override
 	public void delete() {
 		ARBShaderObjects.glDeleteObjectARB(shader);
 	}
-
-	
 
 }
--- src/com/glutilities/shader/ShaderProgram.java
+++ src/com/glutilities/shader/ShaderProgram.java
@@ -1,66 +1,146 @@
 package com.glutilities.shader;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.lwjgl.opengl.ARBFragmentShader;
+import org.lwjgl.opengl.ARBGeometryShader4;
 import org.lwjgl.opengl.ARBShaderObjects;
+import org.lwjgl.opengl.ARBTessellationShader;
+import org.lwjgl.opengl.ARBVertexShader;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL20;
 
 import com.glutilities.core.Reusable;
 
+/**
+ * Represents a shader program which consists of any combination of a vertex
+ * shader, fragment shader, geometry shader, tessellation control shader, or
+ * tessellation evaluation shader.
+ * 
+ * @author Hanavan99
+ *
+ */
 public class ShaderProgram implements Reusable {
 
+	/**
+	 * The program ID of this program.
+	 */
 	private int program;
-	private List<ShaderObject> shaderObjects = new ArrayList<ShaderObject>();
+
+	/**
+	 * The vertex shader object.
+	 */
+	private ShaderObject vertexShader;
+
+	/**
+	 * The fragment shader object.
+	 */
+	private ShaderObject fragmentShader;
+
+	/**
+	 * The geometry shader object.
+	 */
+	private ShaderObject geometryShader;
+
+	/**
+	 * The tessellation control shader.
+	 */
+	private ShaderObject tessControlShader;
+
+	/**
+	 * The tessellation evaluation shader.
+	 */
+	private ShaderObject tessEvalShader;
+
+	/**
+	 * When linked, this string will be the output of the linking process. If there
+	 * is no error, this string will become null. If there is an error, this string
+	 * contains the error information provided by OpenGL.
+	 */
 	private String error;
-	
-	public ShaderProgram() {
-		
+
+	/**
+	 * Creates a new shader program. A shader program can only contain one of each
+	 * type of shader. If you are not using a certain type of shader, set it to
+	 * null.
+	 * 
+	 * @param vertexShader
+	 *            the vertex shader
+	 * @param fragmentShader
+	 *            the fragment shader
+	 * @param geometryShader
+	 *            the geometry shader
+	 * @param tessControlShader
+	 *            the tessellation control shader
+	 * @param tessEvalShader
+	 *            the tessellation evaluation shader
+	 */
+	public ShaderProgram(ShaderObject vertexShader, ShaderObject fragmentShader, ShaderObject geometryShader,
+			ShaderObject tessControlShader, ShaderObject tessEvalShader) {
+		this.vertexShader = vertexShader;
+		this.fragmentShader = fragmentShader;
+		this.geometryShader = geometryShader;
+		this.tessControlShader = tessControlShader;
+		this.tessEvalShader = tessEvalShader;
 	}
-	
-	public void addShaderObject(ShaderObject object) {
-		shaderObjects.add(object);
-	}
-	
+
+	/**
+	 * Returns if the program is linked. Shader programs are not linked until all of
+	 * the shader objects are compiled and attached to the main program.
+	 * 
+	 * @return if the program is linked
+	 */
 	public boolean isLinked() {
-		return ARBShaderObjects.glGetObjectParameteriARB(program, ARBShaderObjects.GL_OBJECT_LINK_STATUS_ARB) == GL11.GL_TRUE;
+		return ARBShaderObjects.glGetObjectParameteriARB(program,
+				ARBShaderObjects.GL_OBJECT_LINK_STATUS_ARB) == GL11.GL_TRUE;
 	}
-	
+
+	/**
+	 * Gets the error text resulting from the linking process.
+	 * 
+	 * @return the error
+	 */
 	public String getError() {
 		return error;
 	}
-	
-	public void compile() {
-		
-		for (ShaderObject object : shaderObjects) {
-			ARBShaderObjects.glShaderSourceARB(object.getShaderID(), object.getShader());
-			ARBShaderObjects.glCompileShaderARB(object.getShaderID());
-			ARBShaderObjects.glAttachObjectARB(program, object.getShaderID());
+
+	/**
+	 * Links the shader program. This process automatically compiles and attaches
+	 * all of the shader objects to the shader program, and then the program is
+	 * linked.
+	 */
+	public void link() {
+		ShaderObject[] shaderObjects = new ShaderObject[] { vertexShader, fragmentShader, geometryShader,
+				tessControlShader, tessEvalShader };
+		int[] shaderTypes = new int[] { ARBVertexShader.GL_VERTEX_SHADER_ARB, ARBFragmentShader.GL_FRAGMENT_SHADER_ARB,
+				ARBGeometryShader4.GL_GEOMETRY_SHADER_ARB, ARBTessellationShader.GL_TESS_CONTROL_SHADER,
+				ARBTessellationShader.GL_TESS_EVALUATION_SHADER };
+
+		for (int i = 0; i < shaderObjects.length; i++) {
+			ShaderObject object = shaderObjects[i];
+			if (object != null) {
+				if (object.getShaderType() != shaderTypes[i])
+				ARBShaderObjects.glShaderSourceARB(object.getShaderID(), object.getShader());
+				ARBShaderObjects.glCompileShaderARB(object.getShaderID());
+				ARBShaderObjects.glAttachObjectARB(program, object.getShaderID());
+			}
 		}
-		
 
-//		fshader = ARBShaderObjects.glCreateShaderObjectARB(ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
-//		ARBShaderObjects.glShaderSourceARB(fshader,
-//				getResourceAsString(new File("src/main/resources/shaders/bloom.fsh")));
-//		ARBShaderObjects.glCompileShaderARB(fshader);
-//		ARBShaderObjects.glAttachObjectARB(program, fshader);
-
-		// tshader =
-		// ARBShaderObjects.glCreateShaderObjectARB(ARBTessellationShader.GL_TESS_EVALUATION_SHADER);
-		// ARBShaderObjects.glShaderSourceARB(tshader,
-		// getResourceAsString("/com/fractal/shader/tessellation_shader.txt"));
-		// ARBShaderObjects.glCompileShaderARB(tshader);
-		// ARBShaderObjects.glAttachObjectARB(program, tshader);
 		ARBShaderObjects.glLinkProgramARB(program);
-		
 		error = ARBShaderObjects.glGetInfoLogARB(program);
 	}
-	
+
+	/**
+	 * Enables the shader program. Any subsequent OpenGL calls will be affected by
+	 * the relevant shader objects. This process continues until {@code disable()}
+	 * is called.
+	 */
 	public void enable() {
 		GL20.glUseProgram(program);
 	}
-	
+
+	/**
+	 * Disables the shader program. Any subsequent OpenGL calls will operate as set
+	 * up before the shader was enabled.
+	 */
 	public void disable() {
 		GL20.glUseProgram(0);
 	}
@@ -69,12 +149,10 @@ public class ShaderProgram implements Reusable {
 	public void create() {
 		program = ARBShaderObjects.glCreateProgramObjectARB();
 	}
-	
+
 	@Override
 	public void delete() {
 		GL20.glDeleteProgram(program);
 	}
 
-	
-	
 }
